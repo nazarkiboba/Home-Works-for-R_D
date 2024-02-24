f79c06d1fbb5340ac1175cf630da5912f7597a25
Назар <luchyk.nazar1512@gmail.com>
2024-02-24 19:58:38 +0200

    HW_19

---
+++ HW real/HW_19/HW_19/HW_19.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include "copy_vector.h"
+
+int main()
+{
+    std::cout << "How many numbers u wont to write: ";
+    int x, How_many_times_do_you_need_to_loop;
+    std::cin >> How_many_times_do_you_need_to_loop;
+    std::cout<< "Enter the tree numbers: ";
+    DynamicIntArray arr;
+    for(int i=0;i<How_many_times_do_you_need_to_loop;i++)
+    {
+        std::cin >> x;
+        arr.append(x);
+    }
+    std::cout << "Your numbers are: ";
+    for (std::size_t i = 0; i < arr.getSize(); ++i) 
+        std::cout << arr[i] << " ";
+    
+    std::cout << "\n";
+
+}
+
